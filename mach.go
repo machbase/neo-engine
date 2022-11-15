06a46d09d38978c080a9aed627d87ddf898986a8
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2022-11-16 08:35:24 +0900

    tidy code

--- mach.go
+++ mach.go
@@ -4,37 +4,6 @@ package mach
 #cgo CFLAGS: -I./native -I.
 #cgo LDFLAGS: -L./native -lmachengine -lpthread -ljemalloc -ldl -lm -lcrypto -Wl,-rpath=./lib
 #include "libmachengine.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-void FetchAllAgain() {
-    void* stmt;
-    nbp_bool_t      fetchEnd = 0;
-
-    MachAllocStmt(&stmt);
-    MachPrepare(stmt, "select * from log");
-    MachExecute(stmt);
-
-    MachFetch(stmt, &fetchEnd);
-
-    while(fetchEnd == NBP_FALSE)
-    {
-        nbp_sint32_t    id;
-        nbp_char_t      name[20] = {0, };
-        nbp_double_t    pre;
-
-        MachGetColumnData(stmt, 0, (void*)&id);
-        MachGetColumnData(stmt, 1, (void*)name);
-        MachGetColumnData(stmt, 2, (void*)&pre);
-
-        printf("id[%d] name[%s] pre[%f]\n", id, name, pre);
-        MachFetch(stmt, &fetchEnd);
-    }
-
-    MachExecuteClean(stmt);
-    MachPrepareClean(stmt);
-    MachFreeStmt(stmt);
-}
 */
 import "C"
 
@@ -62,8 +31,9 @@ func Startup(timeout time.Duration) {
 	C.MachStartupDB(i)
 }
 
-func Shutdown() {
-	C.MachShutdownDB()
+func Shutdown() bool {
+	rt := C.MachShutdownDB()
+	return rt == 0
 }
 
 func IsRunning() bool {
@@ -80,7 +50,9 @@ func ExecuteNewSession(sqlText string) {
 }
 
 func Query(sqlText string, args ...any) (*Rows, error) {
-	rt := Rows{}
+	rt := Rows{
+		sqlText: sqlText,
+	}
 	rc := C.MachAllocStmt(&rt.stmt)
 	if rc != 0 {
 		return nil, errors.New("MachAllocStmt")
@@ -98,8 +70,9 @@ func Query(sqlText string, args ...any) (*Rows, error) {
 }
 
 type Rows struct {
-	stmt unsafe.Pointer
-	eor  C.nbp_bool_t // end of resultset
+	sqlText string
+	stmt    unsafe.Pointer
+	eor     C.nbp_bool_t // end of resultset
 }
 
 func (rows *Rows) Close() {
@@ -157,34 +130,3 @@ func (rows *Rows) Scan(cols ...any) error {
 	}
 	return nil
 }
-
-/*
-func main2() {
-
-	fmt.Println("-------------------------------")
-
-	// Inline C
-	C.MachInitialize(C.CString("/home/eirny/Developer/sample-machdb/tmp/home"))
-
-	C.MachDestroyDB()
-	C.MachCreateDB()
-
-	C.MachStartupDB(10)
-
-	C.MachDirectSQLExecute(C.CString("alter system set trace_log_level=1023"))
-	C.MachDirectSQLExecute(C.CString("create log table log(id int, name varchar(20), pre double)"))
-	C.MachDirectSQLOnNewSession(C.CString("insert into log values(0, 'zero', 1.01)"))
-	C.MachDirectSQLOnNewSession(C.CString("insert into log values(1, 'one', 2.0002)"))
-	C.MachDirectSQLOnNewSession(C.CString("insert into log select id + 20, name, pre *4 from log"))
-
-	C.FetchAll()
-
-	time.Sleep(5 * time.Second)
-
-	C.MachShutdownDB()
-
-	//time.Sleep(10 * time.Second)
-
-	fmt.Println("-------------------------------")
-}
-*/
--- main/machgo/main.go
+++ main/machgo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"mach"
+	"os"
 	"time"
 )
 
@@ -15,6 +16,7 @@ func main() {
 	mach.CreateDatabase()
 
 	mach.Startup(10 * time.Second)
+	defer mach.Shutdown()
 
 	mach.Execute("alter system set trace_log_level=1023")
 	mach.Execute("create log table log(id int, name varchar(20), pre double)")
@@ -25,25 +27,25 @@ func main() {
 
 	fmt.Println("IsRunning", mach.IsRunning())
 
-	if rows, err := mach.Query("select * from log"); err == nil {
-		for rows.Next() {
-			var id int
-			var name string
-			var pre float64
-
-			err := rows.Scan(&id, &name, &pre)
-			if err != nil {
-				fmt.Printf("error: %s]\n", err.Error())
-				break
-			}
-			fmt.Println("---->", id, name, pre)
-		}
-	} else {
+	rows, err := mach.Query("select * from log")
+	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var pre float64
+
+		err := rows.Scan(&id, &name, &pre)
+		if err != nil {
+			fmt.Printf("error: %s]\n", err.Error())
+			break
+		}
+		fmt.Println("---->", id, name, pre)
 	}
 
-	time.Sleep(5)
-
-	mach.Shutdown()
 	fmt.Println("-------------------------------")
 }
