ed224fd37c49acd12002a31c057b73fbaf1fee5f
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2022-12-02 11:57:42 +0900

    fix dummy ar

--- native/dummy/darwin.sh
+++ native/dummy/darwin.sh
@@ -1,2 +1,5 @@
 	gcc -I.. -c machengine_dummy.c
+    if [ -f ../libmachengine_dummy_darwin_amd64.a ]; then
+        rm ../libmachengine_dummy_darwin_amd64.a
+    fi
 	ar rc ../libmachengine_dummy_darwin_amd64.a machengine_dummy.o
