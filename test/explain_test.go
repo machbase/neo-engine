1a99c303f27cb5030d28fd9372e229f981f5c30c
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-05-22 15:16:24 +0900

    add explain test case

---
+++ test/explain_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestExplain(t *testing.T) {
+	plan, err := db.Explain("select * from complex_tag order by time desc", false)
+	require.Nil(t, err)
+	require.True(t, len(plan) > 0)
+	require.True(t, strings.HasPrefix(plan, " PROJECT"))
+	require.True(t, strings.Contains(plan, "KEYVALUE FULL SCAN"))
+	require.True(t, strings.Contains(plan, "VOLATILE FULL SCAN"))
+}
+
+func TestExplainFull(t *testing.T) {
+	plan, err := db.Explain("select * from complex_tag order by time desc", true)
+	require.Nil(t, err)
+	require.True(t, len(plan) > 0)
+	require.True(t, strings.HasPrefix(plan, " PROJECT"))
+	require.True(t, strings.Contains(plan, "KEYVALUE FULL SCAN"))
+	require.True(t, strings.Contains(plan, "VOLATILE FULL SCAN"))
+	t.Log(plan)
+}
