daaf234db72b48a69ed592ac98c9245353f9780e
Kwon, Yeong-Eon <OutOfBedlam@users.noreply.github.com>
2025-07-02 15:35:51 +0900

    Merge pull request #104 from machbase/fix-windows-2022
    fix windows 2022 build env.

--- nativecli/libmachbasecli_linux_amd64.go
+++
@@ -1,9 +0,0 @@
-//go:build linux && amd64
-// +build linux,amd64
-
-package nativecli
-
-// #cgo LDFLAGS: ${SRCDIR}/libmachbasecli_linux_amd64.a
-import "C"
-
-const LibMachLinkInfo = "static_cli_linux_amd64"
