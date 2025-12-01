099069534c7dae3e6ef349e6cc845cff07682b0c
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2025-12-01 11:58:49 +0900

    change DISK_TAG_INDEX_BLOCKS

---
+++ example/macheng/main.go
@@ -0,0 +1,56 @@
+//go:build run
+
+package main
+
+import (
+	_ "embed"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"unsafe"
+
+	mach "github.com/machbase/neo-engine/v8"
+)
+
+//go:embed machbase.conf
+var machbase_conf []byte
+var machPort = 5656
+var machHome = "./tmp/machbase_home"
+
+func main() {
+	homePath, err := filepath.Abs(machHome)
+	if err != nil {
+		panic(err)
+	}
+	confPath := filepath.Join(homePath, "conf", "machbase.conf")
+
+	os.RemoveAll(homePath)
+	os.MkdirAll(homePath, 0755)
+	os.MkdirAll(filepath.Join(homePath, "conf"), 0755)
+	os.MkdirAll(filepath.Join(homePath, "trc"), 0755)
+	os.MkdirAll(filepath.Join(homePath, "dbs"), 0755)
+	os.WriteFile(confPath, machbase_conf, 0644)
+
+	var svrEnvHandle unsafe.Pointer
+	if err := mach.EngInitialize(homePath, machPort, 0x0, &svrEnvHandle); err != nil {
+		panic(err)
+	}
+
+	if !mach.EngExistsDatabase(svrEnvHandle) {
+		mach.EngCreateDatabase(svrEnvHandle)
+	}
+
+	if err := mach.EngStartup(svrEnvHandle); err != nil {
+		panic(err)
+	}
+
+	interruptSignal := make(chan os.Signal, 1)
+	signal.Notify(interruptSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-interruptSignal
+
+	if err := mach.EngShutdown(svrEnvHandle); err != nil {
+		panic(err)
+	}
+	mach.EngFinalize(svrEnvHandle)
+}
