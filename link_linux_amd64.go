dc1698c01de61f546e14002dced74dce97442258
sjkim <andrew.kim@machbase.com>
2026-02-22 12:56:07 +0900

    linux link error fix

--- link_linux_amd64.go
+++ link_linux_amd64.go
@@ -3,7 +3,7 @@
 
 package mach
 
-// #cgo LDFLAGS: ${SRCDIR}/native/libmachengine_standard_linux_amd64.a -lm -ldl -lrt
+// #cgo LDFLAGS: ${SRCDIR}/native/libmachengine_standard_linux_amd64.a -lm -ldl -lrt -lpthread
 import "C"
 
 const LibMachLinkInfo = "static_standard_linux_amd64"
